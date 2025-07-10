aa336ca16ef3a86f9ea3c3c1feb2d35b668dba89
Wesley-Santos-Front <wesleytksantos321@gmail.com>
2025-07-09 23:20:54 -0300

    Responsividade

--- src/pages/servicos.js
+++ src/pages/servicos.js
@@ -6,6 +6,10 @@ import { RxSpeakerOff } from "react-icons/rx";
 import { RiResetLeftFill } from "react-icons/ri";
 import { RxUpdate } from "react-icons/rx";
 import { GrConfigure } from "react-icons/gr";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 
 function Servicos(){
@@ -13,31 +17,31 @@ function Servicos(){
       <div className={style.tit}>
            <h2>Serviços</h2>
            <div className={style.flex}>
-            <div className={style.indiv}>
+            <div className={style.indiv} data-aos="flip-left" data-aos-duration="800">
             <GiSmartphone className={style.icon} />
             <p>Troca de tela</p>
             </div>
-            <div className={style.indiv}>
+            <div className={style.indiv} data-aos="flip-right" data-aos-duration="800">
             <RiBattery2ChargeLine className={style.icon} />
             <p>Troca de bateria</p>
             </div>
-            <div className={style.indiv}>
+            <div className={style.indiv} data-aos="flip-left" data-aos-duration="800">
             <PiVideoCameraSlash className={style.icon} />
             <p>Reparo de câmera</p>
             </div>
-             <div className={style.indiv}>
+             <div className={style.indiv} data-aos="flip-right" data-aos-duration="800">
             <RxSpeakerOff className={style.icon} />
             <p>Reparo no audio</p>
             </div>
-            <div className={style.indiv}>
+            <div className={style.indiv} data-aos="flip-left" data-aos-duration="800">
             <RiResetLeftFill className={style.icon} />
             <p>Hard reset</p>
             </div>
-             <div className={style.indiv}>
+             <div className={style.indiv} data-aos="flip-right" data-aos-duration="800">
             <RxUpdate className={style.icon} />
             <p>Atualização de software</p>
             </div>
-            <div className={style.indiv}>
+            <div className={style.indiv} data-aos="flip-left"  data-aos-duration="800">
             <GrConfigure className={style.icon} />
             <p>E muito mais</p>
             </div>
